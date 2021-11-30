44a132c450cfcfc75c8105331f31fe64a4793ce7
katon <hellokaton@gmail.com>
2021-11-30 19:50:02 +0800

    :sparkles: add websocket chat route

--- src/main/kotlin/me/hellokaton/ktor/routes/ChatRoutes.kt
+++ src/main/kotlin/me/hellokaton/ktor/routes/ChatRoutes.kt
@@ -7,15 +7,33 @@ import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.websocket.*
+import me.hellokaton.ktor.chat.server.Connection
 import me.hellokaton.ktor.models.*
+import java.util.*
+import kotlin.collections.LinkedHashSet
 
 fun Route.chat() {
+    val connections = Collections.synchronizedSet<Connection?>(LinkedHashSet())
+
     webSocket("/chat") {
-        send("建立连接成功!")
-        for (frame in incoming) {
-            frame as? Frame.Text ?: continue
-            val receivedText = frame.readText()
-            send("你发送了: $receivedText")
+        println("有新的连接!")
+        val thisConnection = Connection(this)
+        connections += thisConnection
+        try {
+            send("你已建立连接! 你是第 ${connections.count()} 个用户.")
+            for (frame in incoming) {
+                frame as? Frame.Text ?: continue
+                val receivedText = frame.readText()
+                val textWithUsername = "[${thisConnection.name}]: $receivedText"
+                connections.forEach {
+                    it.session.send(textWithUsername)
+                }
+            }
+        } catch (e: Exception) {
+            println(e.localizedMessage)
+        } finally {
+            println("断开连接: $thisConnection!")
+            connections -= thisConnection
         }
     }
 }
